be73f06adbcf7760176e601b43a1d33cc6e63366
LBenzahia <lakhdar.benzahia@gmail.com>
2017-11-24 22:33:00 +0100

    complete the 1st section python basics

---
+++ section-0/1_variables_methods.py
@@ -0,0 +1,24 @@
+a = 5
+b = 9
+
+my_variable = 125
+my_10_variable = 10
+
+string_variable = "Hi Lakhdar!"
+single_quotes = 'String can have signle quates'
+
+print(my_variable)
+print(string_variable)
+
+## Methods
+
+def my_print_method(my_argument):
+    print(my_argument)
+
+def my_multiply_method(num_one, num_two):
+    return num_one * num_two
+
+result = my_multiply_method(10, 3)
+print(result)
+
+my_print_method(my_multiply_method(10, 3))
